0e491b290a7c36ba46faff6cdcb380e0e273b10d
Matthew Torres <matthewt@meditab.com>
2015-01-28 21:45:50 +0800

    add coffee and sass converter

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
 	grunt.registerTask('compileAssets', [
 		'clean:dev',
     'jade:dev',
+    'sass:dev',
 		'jst:dev',
 		'copy:dev',
 		'coffee:dev'
--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -1,8 +1,9 @@
 module.exports = function (grunt) {
 	grunt.registerTask('syncAssets', [
 		'jst:dev',
-		'less:dev',
+		'sass:dev',
 		'sync:dev',
-		'coffee:dev'
+		'coffee:dev',
+    'jade:dev'
 	]);
 };
